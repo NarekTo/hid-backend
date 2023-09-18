4be5a4adf263345bcad5e8f1848f5673ceaad9d8
serenahg <serena.o@hidgroup.eu>
2023-09-18 17:14:00 +0200

    items table module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { ProjectBatchesModule } from './project_batches/project_batches.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersService } from './users/users.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [ProjectBatchesModule, AuthModule, PrismaModule],
+  imports: [ProjectBatchesModule, AuthModule, PrismaModule, ItemsModule],
   controllers: [],
   providers: [UsersService],
 })
