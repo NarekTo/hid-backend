23ed1c57e62e5544de38c42b6ae7d41b6aacc1c1
serenahg <serena.o@hidgroup.eu>
2023-10-04 15:19:27 +0200

    updated prisma + new columns in local sql added for status_code column and prisma db pull + prisma generate done to update in backend prisma.schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { UsersService } from './users/users.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ItemsModule } from './items/items.module';
+import { ProjectProjectsModule } from './project_projects/project_projects.module';
 
 @Module({
-  imports: [ProjectBatchesModule, AuthModule, PrismaModule, ItemsModule],
+  imports: [ProjectBatchesModule, AuthModule, PrismaModule, ItemsModule, ProjectProjectsModule],
   controllers: [],
   providers: [UsersService],
 })
--- src/project_projects/project_projects.module.ts
+++ src/project_projects/project_projects.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProjectProjectsController } from './project_projects.controller';
 import { ProjectProjectsService } from './project_projects.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
+
+
   controllers: [ProjectProjectsController],
-  providers: [ProjectProjectsService],
+  providers: [ProjectProjectsService, PrismaService],
 })
 export class ProjectProjectsModule {}
